d64e73e04ea657e27a5b5191dd40c347af549097
yeasinbinali <yeasinbinali@gmail.com>
2024-03-10 18:08:12 +0600

    header part added

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Countries from './main/Countries';
+import Header from './main/Header';
+
+function App() {
+
+  return (
+    <>
+      <Header></Header>
+      <Countries></Countries>
+    </>
+  )
+}
+
+export default App;
---
+++ src/main/Countries.jsx
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+import { useState } from "react"
+
+export default function Countries() {
+    const [countries, setCountries] = useState([]);
+
+    useEffect(() => {
+        fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => setCountries(data))
+    }, [])
+
+    return (
+        <>
+            {
+                countries.map(country => console.log(country))
+            }
+        </>
+    )
+
+}
