ba5b87704e639c71dbdf4047c6c4bd252f0b2bf8
yeasinbinali <yeasinbinali@gmail.com>
2024-03-13 19:24:54 +0600

    country-modal added

--- src/main/Countries.jsx
+++ src/main/Countries.jsx
@@ -4,17 +4,29 @@ import Country from "./Country";
 
 export default function Countries() {
     const [countries, setCountries] = useState([]);
+    const [displayCountry, setDisplayCountry] = useState([]);
 
     useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
-        .then(res => res.json())
-        .then(data => setCountries(data))
+            .then(res => res.json())
+            .then(data => setCountries(data))
     }, [])
+
+    const handleDisplayCountry = (code) => {
+        fetch(`https://restcountries.com/v3.1/alpha/${code}`)
+            .then(res => res.json())
+            .then(data => setDisplayCountry(data[0]))
+    }
 
     return (
         <div className="w-[95%] mx-auto grid grid-cols-4 gap-10 my-10">
             {
-                countries.map(country => <Country country = {country}></Country>)
+                countries.map(country => <Country
+                    key={country.cca3}
+                    country={country}
+                    handleDisplayCountry={handleDisplayCountry}
+                    displayCountry={displayCountry}
+                ></Country>)
             }
         </div>
     )
--- src/main/Country.jsx
+++ src/main/Country.jsx
@@ -1,18 +1,8 @@
-import { useState } from "react";
-
-export default function Country({ country }) {
-    // console.log(country.name.common);
+export default function Country({ country, handleDisplayCountry, displayCountry }) {
     const flag = country.flags.png;
     const countryName = country.name.common;
+    const countryCca3 = country.cca3;
 
-    const [displayCountry, setDisplayCountry] = useState([]);
-
-    const handleCountryDetails = async (name) => {
-        const country = `https://restcountries.com/v3.1/name/${name}`;
-        const res = await fetch(country);
-        const data = await res.json();
-        setDisplayCountry(data[0]);
-    }
     console.log(displayCountry);
 
     return (
@@ -20,31 +10,32 @@ export default function Country({ country }) {
             <div style={{ border: '1px solid #EEEEEE', borderRadius: '15px' }} className="p-5 text-center">
                 <img className="w-40 h-20 mx-auto rounded-lg" src={flag} alt="flags" />
                 <h4 className="text-black text-2xl text-center font-bold my-3 font-sans">{countryName}</h4>
-                <button
-                    className="btn btn-outline btn-sm"
-                    onClick={() => {
-                        handleCountryDetails(countryName);
-                        document.getElementById('my_modal_5').showModal();
-                    }}
-                >
-                    View Details
-                </button>
-                <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
-                    <div className="modal-box text-black">
-                        <h3 className="font-bold text-black text-lg">Hello</h3>
-                        <p className="py-4">Press ESC key or click the button below to close</p>
-                        <div className="modal-action">
-                            <form method="dialog">
-                                <button className="btn">Close</button>
-                            </form>
+                <div>
+                    <button
+                        className="btn btn-outline btn-sm"
+                        onClick={() => {
+                            handleDisplayCountry(countryCca3);
+                            document.getElementById('my_modal_5').showModal();
+                        }}
+                    >
+                        View Details
+                    </button>
+                    <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+                        <div className="modal-box text-black">
+                            <img className="w-40 h-20 mx-auto rounded-lg" src={displayCountry?.flags?.png} alt="flags" />
+                            <h3 className="font-bold text-black text-4xl mt-3">{displayCountry?.name?.common}</h3>
+                            <p className="mt-2">Capital: {displayCountry?.capital}</p>
+                            <p className="mt-2">Region: {displayCountry?.region}</p>
+                            <p className="mt-2">Population: {displayCountry?.population}</p>
+                            <div className="modal-action">
+                                <form method="dialog">
+                                    <button className="btn">Close</button>
+                                </form>
+                            </div>
                         </div>
-                    </div>
-                </dialog>
+                    </dialog>
+                </div>
             </div>
-
-            <script>
-
-            </script>
         </>
     )
 
